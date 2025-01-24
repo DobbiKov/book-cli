4900d4929a7b8f1c406a05e4ef47d720c3135460
Yehor Korotenko <dobbikov@gmail.com>
2025-01-24 20:53:19 +0100

    made it just book-cli app dependent on book-lib

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
-mod book;
-mod db;
-use std::{
-    fs,
-    path::{Path, PathBuf},
-    process,
-};
-
+use book_lib::{book, db, help};
 use clap::{error::ErrorKind, CommandFactory, Parser, Subcommand};
+use std::process;
 
 #[derive(Parser)]
 #[command(name = "Book CLI")]
@@ -40,35 +34,6 @@ enum Commands {
         #[arg(value_hint=clap::ValueHint::Other)]
         name: String,
     },
-}
-
-fn is_pdf(path: &str) -> bool {
-    let str_path = path;
-    match str_path.split(".").last() {
-        Some(val) => val == "pdf",
-        None => false,
-    }
-}
-fn is_correct_path(path: &String) -> (bool, Option<PathBuf>) {
-    let path = Path::new(path);
-    if !path.exists() {
-        (false, None)
-    } else {
-        (true, Some(fs::canonicalize(path).unwrap()))
-    }
-}
-
-// returns a list of books that are only of particular sections
-fn get_books_with_section(books: Vec<book::Book>, section: &String) -> Vec<book::Book> {
-    let mut res: Vec<book::Book> = Vec::new();
-    for bk in books {
-        if let Some(bk_sec) = &bk.section {
-            if bk_sec == section {
-                res.push(bk);
-            }
-        }
-    }
-    res
 }
 
 fn print_by_section(books_by_sec: Vec<(String, Vec<book::Book>)>) {
@@ -106,7 +71,7 @@ fn main() {
                 },
             };
             if let Some(section) = section {
-                let sorted_by_section = get_books_with_section(books, section);
+                let sorted_by_section = help::get_books_with_section(books, section);
                 println!("Section: [{0}]", section);
                 book::print_books(sorted_by_section, 4);
             } else if *sort_by_section {
@@ -122,11 +87,11 @@ fn main() {
             section,
         } => {
             let mut cmd = Cli::command();
-            if !is_pdf(path) {
+            if !help::is_pdf(path) {
                 cmd.error(clap::error::ErrorKind::InvalidValue, "The file is not PDF!")
                     .exit();
             }
-            let (is_correct, good_path) = is_correct_path(path);
+            let (is_correct, good_path) = help::is_correct_path(path);
             if !is_correct {
                 cmd.error(clap::error::ErrorKind::InvalidValue, "Invalid path!")
                     .exit();
